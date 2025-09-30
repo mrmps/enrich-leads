58da1d8c9902a34340dd1fea7f77a4a68467e8de
mike <sfr.michael@yahoo.com>
2025-09-29 17:21:34 -0700

    Fix: Remove TypeScript any type for Cloudflare build

--- src/db/index.ts
+++ src/db/index.ts
@@ -18,6 +18,6 @@ export const db = new Proxy({} as ReturnType<typeof getDb>, {
     if (!_db) {
       _db = getDb();
     }
-    return (_db as any)[prop];
+    return (_db as Record<string, unknown>)[prop as string];
   }
 });
